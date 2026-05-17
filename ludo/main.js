de5da85739bb5e57539960849c02dcf87dd5624e
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2026-05-17 17:03:34 +0530

    Ludo board design complete

--- ludo/main.js
+++ ludo/main.js
@@ -1,8 +1,26 @@
 const app = Vue.createApp({
   data() {
     return {
-      msg: 'hello akka',
+      colors: ['red', 'green', 'yellow', 'blue'],
     };
+  },
+
+  methods: {
+    shouldVerticalGalleryForColor(color) {
+      return ['green', 'blue'].includes(color);
+    },
+    shouldCellHaveStar(color, pathN, cellN) {
+      if (color == 'red' && pathN == 3 && cellN == 2) return true;
+      if (color == 'green' && pathN == 1 && cellN == 2) return true;
+      if (color == 'yellow' && pathN == 1 && cellN == 5) return true;
+      if (color == 'blue' && pathN == 3 && cellN == 5) return true;
+    },
+    shouldCellHaveArrow(color, pathN, cellN) {
+      if (color == 'red' && pathN == 2 && cellN == 1) return true;
+      if (color == 'green' && pathN == 2 && cellN == 1) return true;
+      if (color == 'yellow' && pathN == 2 && cellN == 6) return true;
+      if (color == 'blue' && pathN == 2 && cellN == 6) return true;
+    },
   },
 });
 
